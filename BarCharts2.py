11daf0031afb7bf70bf5e08ad1f15ba4f5d3219f
andhikaswitch <andhikaswitch@gmail.com>
2024-11-03 23:25:29 +0700

    Add files via upload

---
+++ BarCharts2.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+countries = ('Brunei', 'cambodia', 'Indonesia', 'Laos', 'Malaysia', 'Myanmar', 'Philippines', 'Singapore', 'Thailand', 'Vietnam')
+cases = (135430, 135714, 6018048, 181967, 4219395, 611674, 3678968, 1101438, 3684755, 9716282)
+x_coords = np.arange(len(countries))
+plt.bar(x_coords, cases, tick_label = countries)
+plt.xticks(rotation=90)
+plt.ylabel('Total Covid Cases')
+plt.title('Covid Cases in ASEAN')
+plt.show()
---
+++ BarCharts4.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+countries = ('Brunei', 'cambodia', 'Indonesia', 'Laos', 'Malaysia', 'Myanmar', 'Philippines', 'Singapore', 'Thailand', 'Vietnam')
+cases = (135430, 135714, 6018048, 181967, 4219395, 611674, 3678968, 1101438, 3684755, 9716282)
+
+df = pd.DataFrame ({
+    'Country':countries,
+    'Case': cases,
+})
+df.sort_values(by='Case', inplace=True)
+
+x_coords = np.arange(len(df))
+colors = ['#0000FF' for _ in range(len(df))]
+colors[-2] = '#FF0000'
+plt.bar(x_coords, df['Case'], tick_label = df['Country'], color=colors)
+plt.xticks(rotation=90)
+plt.ylabel('Total Covid Cases')
+plt.title('Covid Cases in ASEAN')
+plt.show()
